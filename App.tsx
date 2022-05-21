3b728df0443c6047c95efc94f86f3519cef71ed9
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-05-21 18:04:30 -0300

    Add svg

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
+import React from 'react';
+import { View, Button } from 'react-native';
+import Svg, { Circle, Rect } from 'react-native-svg';
 import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedStyle,
   Easing,
 } from 'react-native-reanimated';
-import { View, Button } from 'react-native';
-import React from 'react';
+
 
 export default function AnimatedStyleUpdateExample(props) {
   const randomWidth = useSharedValue(10);
@@ -22,22 +24,9 @@ export default function AnimatedStyleUpdateExample(props) {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[{ width: 100, height: 80, backgroundColor: 'black', margin: 30 }, style]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
-    </View>
+    <Svg height="50%" width="50%" viewBox="0 0 100 100" {...props}>
+      <Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
+      <Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
+    </Svg>
   );
 }
