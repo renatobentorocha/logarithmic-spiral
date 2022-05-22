ab13c7404dfa94d2a6a8f27e2e4a2c26f12233da
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-05-21 23:20:14 -0300

    Add prettier

--- App.tsx
+++ App.tsx
@@ -2,33 +2,48 @@ import React from 'react';
 import { View, Button } from 'react-native';
 import Svg, { Circle, Rect } from 'react-native-svg';
 import Animated, {
-	useSharedValue,
-	withTiming,
-	useAnimatedStyle,
-	Easing,
+  useSharedValue,
+  withTiming,
+  useAnimatedStyle,
+  Easing,
 } from 'react-native-reanimated';
 
-
 export default function AnimatedStyleUpdateExample(props) {
-	const randomWidth = useSharedValue(10);
+  const randomWidth = useSharedValue(10);
 
-	const config = {
-		duration: 500,
-		easing: Easing.bezier(0.5, 0.01, 0, 1),
-	};
+  const a = '';
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  };
 
-	const style = useAnimatedStyle(() => {
-		return {
-			width: withTiming(randomWidth.value, config),
-		};
-	});
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    };
+  });
 
-	return (
-		<>
-			<Svg height="50%" width="50%" viewBox="0 0 100 100" {...props}>
-				<Circle cx="50" cy="50" r="45" stroke="blue" strokeWidth="2.5" fill="green" />
-				<Rect x="15" y="15" width="70" height="70" stroke="red" strokeWidth="2" fill="yellow" />
-			</Svg>
-		</>
-	);
+  return (
+    <>
+      <Svg height="50%" width="50%" viewBox="0 0 100 100" {...props}>
+        <Circle
+          cx="50"
+          cy="50"
+          r="45"
+          stroke="blue"
+          strokeWidth="2.5"
+          fill="green"
+        />
+        <Rect
+          x="15"
+          y="15"
+          width="70"
+          height="70"
+          stroke="red"
+          strokeWidth="2"
+          fill="yellow"
+        />
+      </Svg>
+    </>
+  );
 }
