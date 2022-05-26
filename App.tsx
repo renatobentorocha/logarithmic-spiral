6ce9214f4dd28445d10abcbdbdb00cc6c8bab315
reanatorocha <87495675+reanatorocha@users.noreply.github.com>
2022-05-26 18:34:04 -0300

    (WIP) - Initial animation

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, useWindowDimensions } from 'react-native';
 import Svg, { Polyline, PolylineProps } from 'react-native-svg';
 import Animated, {
   useSharedValue,
   withTiming,
   useAnimatedProps,
-  runOnUI,
+  useAnimatedStyle,
+  useAnimatedReaction,
+  interpolate,
+  runOnJS,
+  withSpring,
+  withRepeat,
+  Extrapolate,
 } from 'react-native-reanimated';
 
 const AnimatedPolyline = Animated.createAnimatedComponent(Polyline);
 
-const POINT_SIZE = 5;
+const POINT_SIZE = 10;
 const FULL_CIRCLE = 2 * Math.PI;
 const NUMBER_OF_CIRCLES = 12;
 
@@ -32,13 +38,74 @@ function logarithmicSpiralPoint(a: number, k: number, ɸ: number) {
   return { x, y };
 }
 
+type PointType = {
+  center: { x: number; y: number };
+  x: number;
+  y: number;
+};
+
+function Point({ center, x, y }: PointType) {
+  const sharedValue = useSharedValue(1);
+
+  sharedValue.value = withRepeat(
+    withTiming(1000, { duration: 1000 }),
+    -1,
+    true
+  );
+
+  const styles = useAnimatedStyle(() => {
+    const opacity = interpolate(
+      sharedValue.value,
+      [0, 250, 500, 750, 1000],
+      [0, 0.25, 0.5, 0.75, 1],
+      Extrapolate.CLAMP
+    );
+
+    return {
+      opacity,
+      transform: [
+        { translateX: withSpring(center.x + x) },
+        { translateY: withSpring(center.y + y) },
+        { scale: opacity + 0.3 },
+      ],
+    };
+  });
+
+  return (
+    <Animated.View
+      style={[
+        {
+          position: 'absolute',
+          width: POINT_SIZE,
+          height: POINT_SIZE,
+          borderRadius: POINT_SIZE / 2,
+          backgroundColor: 'white',
+        },
+        styles,
+      ]}
+    />
+  );
+}
+
 export default function App() {
   const { width, height } = useWindowDimensions();
 
+  const [showPoints, setShowPoints] = useState(false);
+
   const x = useSharedValue(0);
   const y = useSharedValue(0);
+  const step = useSharedValue(0);
+
+  const points = useSharedValue<Array<number[]>>([[0, 0]]);
+
   const animatedPoints = useSharedValue<Array<number>>([0, 0]);
   const incrementAngle = useSharedValue(0.1);
+
+  function animateBall() {
+    'worklet';
+
+    runOnJS(setShowPoints)(true);
+  }
 
   function draw() {
     'worklet';
@@ -50,23 +117,46 @@ export default function App() {
       const { x, y } = logarithmicSpiralPoint(a, b, angle);
 
       animatedPoints.value = [...animatedPoints.value, x, y];
+      points.value = [...points.value, [x, y]];
+
       angle = angle + incrementAngle.value;
     }
   }
 
   function goToCenter() {
     'worklet';
-    x.value = width / 2 - POINT_SIZE / 2;
-    y.value = height / 2 - POINT_SIZE / 2;
+    x.value = width / 2;
+    y.value = height / 2;
   }
 
-  useEffect(() => {
-    runOnUI(() => {
-      'worklet';
-      goToCenter();
-      draw();
-    })();
-  }, []);
+  useAnimatedReaction(
+    () => {
+      return step.value;
+    },
+    (data, prev) => {
+      if (data === prev) return;
+
+      switch (data) {
+        case 0:
+          goToCenter();
+          step.value = withTiming(1);
+          break;
+
+        case 1:
+          draw();
+          step.value = withTiming(2);
+          break;
+
+        case 2:
+          animateBall();
+          step.value = withTiming(3);
+          break;
+
+        default:
+          break;
+      }
+    }
+  );
 
   const polylineProps = useAnimatedProps<PolylineProps>(() => {
     return {
@@ -90,6 +180,15 @@ export default function App() {
         stroke="black"
         strokeWidth="3"
       />
+      {showPoints &&
+        points.value.map((p, index) => (
+          <Point
+            key={index.toString()}
+            center={{ x: x.value - 10, y: y.value - 10 }}
+            x={p[0]}
+            y={p[1]}
+          />
+        ))}
     </Svg>
   );
 }
